711ab69be6a4646baab2057aca12ed08c3884569
jasonhzhang <jasonhzhang@tencent.com>
2026-05-27 14:34:11 +0800

    chore(release): v0.1.9

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import eslint from '@eslint/js';
 import globals from 'globals';
 import prettier from 'eslint-plugin-prettier/recommended';
 import tseslint from 'typescript-eslint';
-
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
